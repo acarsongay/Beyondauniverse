e12e36747d346e6d90d0418ab2270ba8282a0cda
Andrew <acarsongay@gmail.com>
2019-02-28 10:23:30 -0600

    Testing react

--- scripts/client.js
+++ scripts/client.js
@@ -3,7 +3,7 @@
 //	"function_get_json_string_url" : "./story.js",
 //	"function_get_json_callback_success" : "function( data ){ console.log( data ) }"
 // } ) );
-requirejs(["scripts/helper/functions"], function( functions ) {
+requirejs(["helper/functions"], function( functions ) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
