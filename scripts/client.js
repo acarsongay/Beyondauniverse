f5c96b02ae70e35945d7a5340c4f54f44ab4e82b
Andrew <acarsongay@gmail.com>
2019-02-28 10:18:59 -0600

    Testing react

--- scripts/client.js
+++ scripts/client.js
@@ -9,4 +9,8 @@ requirejs(["scripts/helper/functions"], function( functions ) {
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
     //the module value for "helper/util".
+	console.log( functions.function_get_json( {
+		"function_get_json_string_url":"base_types.json",
+		"function_get_json_callback_success": "functions.json_flatten"
+	}) );
 });
--- scripts/helper/functions.js
+++ scripts/helper/functions.js
@@ -127,5 +127,6 @@ function function_get_json( placeholder_json ) {
 
 module.exports = {
 	function_get_json: function_get_json
+	json_flatten: json_flatten
 };
 
