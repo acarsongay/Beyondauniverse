d17dadb141fbaff066b0596bb0963566c2406ee3
Andrew <acarsongay@gmail.com>
2019-03-02 10:25:19 -0600

    pushing to github

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 import { client } from './client.js';
 
-var user_client = new client( { "type":"block", "label": "", "placeholder_json": {} } );
-user_client.launch();
-console.log(  user_client.json.launch_results );
+var user_client = new client( { "type":"block", "label": "", "placeholder_json": {} , "jquery_ajax_url":"./base_types.json" } );
 
+console.log ( user_client.json );
 
+console.log ( user_client.merge_json_and_placeholder_json() );
+
+console.log ( user_client.merge_json_with ( {"type":"updated_block"} ) );
+
+console.log ( user_client.merge_json_with_no_override ( {"type":"updated_block_unknown","did":"did"} ) );
+
+console.log ( user_client.json_stringify( ) );
+
+console.log ( user_client.json_flatten( ) );
+
+console.log ( user_client.jquery_ajax( ) );
+
+console.log ( user_client.merge_json_with_jquery_ajax( ) );
+
+console.log ( user_client.json );
 
